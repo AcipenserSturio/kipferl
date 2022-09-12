41d9227b52a28d6487f87247ebad2172961d03b0
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2022-09-12 18:32:43 +0300

    Fixed arrows issue

--- src/game.py
+++ src/game.py
@@ -9,8 +9,6 @@ class Game:
 
     def run(self):
         with CursesContextManager() as stdscr:
-            stdscr.clear()
-            stdscr.refresh()
 
             curses.init_pair(255, 255, 69)
 
@@ -20,27 +18,16 @@ class Game:
 
             while True:
                 pad.timeout(10)
-                key = stdscr.getch()
-                self.handle(key)
-                self.testprint(pad, str(key))
-                self.testprint(pad, str(curses.KEY_LEFT))
+                self.handle(stdscr.getch())
                 pad.refresh(self.y_offset, self.x_offset, 0, 0, 30, 70)
 
     def handle(self, key):
         match key:
             case curses.KEY_LEFT:
-                self.testprint(pad)
                 self.x_offset += -2
             case curses.KEY_RIGHT:
-                self.testprint(pad)
                 self.x_offset += 2
             case curses.KEY_UP:
-                self.testprint(pad)
                 self.y_offset += -1
             case curses.KEY_DOWN:
-                self.testprint(pad)
                 self.y_offset += 1
-
-    def testprint(self, win, message):
-        win.addstr(message + " ", curses.color_pair(255))
-
--- src/utils.py
+++ src/utils.py
@@ -11,10 +11,15 @@ class CursesContextManager:
         self.stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
+        self.stdscr.keypad(1)
         try:
             curses.start_color()
         except:
             pass
+        # Clearing and refreshing is not in the official wrapper:
+        # However, there is no harm in doing it as early as possible.
+        self.stdscr.clear()
+        self.stdscr.refresh()
         return self.stdscr
 
     def __exit__(self, ex_type, ex_value, ex_traceback):
