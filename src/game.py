1fda71046394132bfbfd5a75d14504ad4e996387
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2022-09-13 19:13:52 +0300

    Display follows player

--- src/game.py
+++ src/game.py
@@ -22,13 +22,11 @@ class Game:
             self.level.load("assets/levels/huge")
             self.level.build()
 
-            pad = curses.newpad(self.level.lines, self.level.cols)
-            self.level.draw(pad)
-            self.level.display = pad
+            self.display = curses.newwin(curses.LINES, curses.COLS)
 
             while not self.game_quit:
-                pad.refresh(self.y_offset, self.x_offset, 0, 0, curses.LINES-1, curses.COLS-1)
-                pad.timeout(10)
+                self.refresh()
+                self.display.timeout(10)
                 self.handle(stdscr.getch())
 
     def handle(self, key):
@@ -43,3 +41,22 @@ class Game:
                 self.level.player.walk("d")
             case 113: # q
                 self.game_quit = True
+
+    def refresh(self):
+        self.x_offset = self.level.player.cell.x
+        self.y_offset = self.level.player.cell.y
+        for y in range(curses.LINES):
+            for x in range(curses.COLS):
+                cell = self.level.get_cell(y+self.y_offset, x+self.x_offset)
+                if cell:
+                    self.put(cell, y, x)
+        self.display.refresh()
+
+    def put(self, cell, y, x):
+        # Edge case last character. todo: fix
+        if (y == curses.LINES-1) and (x == curses.COLS-1):
+            return
+        color = curses.color_pair(cell.color)
+        char = cell.character.char if cell.character else " "
+        self.display.move(y, x)
+        self.display.addch(char, color)
