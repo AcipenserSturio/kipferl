78c3e83a4c2ecde374b304482212a4e3a19a193e
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2022-10-12 17:31:39 +0300

    Use threads for the sound engine

--- src/sound.py
+++ src/sound.py
@@ -1,8 +1,13 @@
 from pathlib import Path
+from threading import Thread
 
 from playsound import playsound
 
 def play(sound):
     if not sound:
         return
-    playsound(Path().cwd() / "assets" / "sound" / sound, False)
+    thread = Thread(target=_play, args=(sound,))
+    thread.start()
+
+def _play(sound):
+    playsound(Path().cwd() / "assets" / "sound" / sound, block=True)
