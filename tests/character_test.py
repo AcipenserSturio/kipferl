5a9609fd85bdd51a772542791c1e811201d1a1fe
KarinaZakirova <90447262+KarinaZakirova@users.noreply.github.com>
2022-10-27 15:45:36 +0300

    test types character (fix problem)

--- tests/character_test.py
+++ tests/character_test.py
@@ -5,7 +5,7 @@ from src.game import Game
 class CharacterTest(unittest.TestCase):
 
     def setUp(self):
-        self.game = Game(self)
+        self.game = Game()
 
     def test_types(self):
         assert isinstance(self.game.level.enemies, set)
