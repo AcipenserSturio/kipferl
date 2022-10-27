017cfd7f6421e824a86908763f83f85e11e8e50e
KarinaZakirova <90447262+KarinaZakirova@users.noreply.github.com>
2022-10-27 16:06:46 +0300

    divide test types character

--- tests/character_test.py
+++ tests/character_test.py
@@ -11,9 +11,14 @@ class CharacterTest(unittest.TestCase):
     def setUp(self):
         self.game = Game()
 
-    def test_types(self):
+    def test_enemies_types(self):
         assert isinstance(self.game.level.enemies, set)
+
+    def test_enemy_types(self):
         for enemy in self.game.level.enemies:
             assert isinstance(enemy, Character)
             assert isinstance(enemy, Enemy)
             assert not isinstance(enemy, Player)
+
+    def test_player_types(self):
+        assert isinstance(self.game.level.player, Player)
